7fe9f49b8f77ac646ea2f0b47fb3d9480f8026d0
Shafahid <164517858+Shafahid@users.noreply.github.com>
2024-11-25 20:37:15 +0600

    Update Mergesort.cpp

--- Recursion/Mergesort.cpp
+++ Recursion/Mergesort.cpp
@@ -1,8 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 1e5+10;
-int a[N];
-void merged(int l,int r,int mid){
+void merged(int a[],int l,int r,int mid){
 int l_sz = mid-l+1;
 int L[l_sz];
 int r_sz = r-mid;
@@ -13,36 +11,37 @@ for(int i=0;i<l_sz;i++){
 for(int i=0;i<r_sz;i++){
     R[i] = a[i+mid+1];
 }
-L[l_sz]=R[r_sz]=INT_MAX;
 int l_i=0;
 int r_i=0;
-for(int i=l;i<=r;i++){
-    if(L[l_i]<=R[r_i]){
-        a[i] = L[l_i];
-        l_i++;
+int k=l;
+while(l_i<l_sz&&r_i<r_sz){
+    if(L[l_i]<R[r_i]){
+        a[k++] = L[l_i++];
     }else{
-    a[i] = R[r_i];
-    r_i++;
+        a[k++] = R[r_i++];
     }
+}
+while(l_i<l_sz) a[k++] = L[l_i++];
+while(r_i<r_sz) a[k++] = R[r_i++];
 
 }
-}
-void mergesort(int l,int r){
+void mergesort(int a[],int l,int r){
 if(l==r) return;
 int mid = (l+r)/2;
-mergesort(l,mid);
-mergesort(mid+1,r);
-merged(l,r,mid);
+mergesort(a,l,mid);
+mergesort(a,mid+1,r);
+merged(a,l,r,mid);
 
 }
 int main(){
 int n;
 cin >> n;
+int a[n];
 for(int i=0;i<n;i++){
 cin >> a[i];
 
 }
-mergesort(0,n-1);
+mergesort(a,0,n-1);
 for(int i=0;i<n;i++){
 cout << a[i] << " ";
 }
