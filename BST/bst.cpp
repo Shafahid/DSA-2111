a3be605d174cf06c204dc0d77ed6a5b2b0139663
Shafahid <164517858+Shafahid@users.noreply.github.com>
2025-01-03 22:56:41 +0600

    Update bst.cpp

--- BST/bst.cpp
+++ BST/bst.cpp
@@ -109,65 +109,84 @@ Node* maxValueNode(Node* root) {
     return curr;
 }
 
-void delete_node(Node *root, int key)
+Node *removeNode(Node *root, int val)
 {
-    Node *d = search(root, key);
-    if (d)
-    { // if the node exists
-
-        // Case-1: leaf node
-        if (d->left == NULL && d->right == NULL)
+    /*
+     * If the node becomes NULL, it will return NULL
+     * Two possible ways which can trigger this case
+     * 1. If we send the empty tree. i.e root == NULL
+     * 2. If the given node is not present in the tree.
+     */
+    if(root == NULL)
+        return NULL;
+    /*
+     * If root->key < val. val must be present in the right subtree
+     * So, call the above remove function with root->right
+     */
+    if(root->key < val)
+        root->right = removeNode(root->right,val);
+    /*
+     * if root->key > val. val must be present in the left subtree
+     * So, call the above function with root->left
+     */
+    else if(root->key > val)
+        root->left = removeNode(root->left,val);
+    /*
+     * This part will be executed only if the root->key == val
+     * The actual removal starts from here
+     */
+    else
+    {
+        /*
+         * Case 1: Leaf node. Both left and right reference is NULL
+         * replace the node with NULL by returning NULL to the calling pointer.
+         * free the node
+         */
+        if(root->left == NULL && root->right == NULL)
         {
-
-            if (d->parent)
-            {
-                if (d->parent->key > d->key)
-                    d->parent->left = NULL;
-                else
-                    d->parent->right = NULL;
-            }
-
-            d = NULL;
-            delete d;
+            free(root);
+            return NULL;
         }
-
-        // Case-2: if there is one child
-        else if (d->left == NULL)
-        { // right child exists
-            if (d->parent)
-            {
-
-                if (d->parent->key < d->key)
-                    d->parent->right = d->right;
-                else
-                    d->parent->left = d->right;
-
-                d = NULL;
-                delete d;
-            }
+        /*
+         * Case 2: Node has right child.
+         * replace the root node with root->right and free the right node
+         */
+        else if(root->left == NULL)
+        {
+            Node *temp = root->right;
+            free(root);
+            return temp;
         }
-        else if (d->right == NULL)
-        { // left child exists
-            if (d->parent)
-            {
-                if (d->parent->key < d->key)
-                    d->parent->right = d->left;
-                else
-                    d->parent->left = d->left;
-
-                d = NULL;
-                delete d;
-            }
+        /*
+         * Case 3: Node has left child.
+         * replace the node with root->left and free the left node
+         */
+        else if(root->right == NULL)
+        {
+            Node *temp = root->left;
+            free(root);
+            return temp;
         }
-        //Case-3: there are two children
+        /*
+         * Case 4: Node has both left and right children.
+         * Find the min value in the right subtree
+         * replace node value with min.
+         * And again call the remove function to delete the node which has the min value.
+         * Since we find the min value from the right subtree call the remove function with root->right.
+         */
         else
         {
-            Node *temp = minValueNode(d->right);
-            root->key = temp->key;
-            delete_node(root->right,temp->key);
+            int rightMin = minValueNode(root->right)->key;
+            root->key = rightMin;
+            root->right = removeNode(root->right,rightMin);
         }
+
     }
+
+    //return the actual root's address
+    return root;
 }
+
 
 void inorder(Node *root)
 {
@@ -264,7 +283,11 @@ int main()
     BST = insert(BST, 60);
     BST = insert(BST, 80);
     inorder(BST);
+     cout<<endl;
+     removeNode(BST,50);
+     cout<<endl;
+     inorder(BST);
+
     return 0;
 }
-
 
